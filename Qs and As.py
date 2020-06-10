9e733e8026f9091bcb55af4932938571718a0d8c
jinokwon <55212787+jinokwon@users.noreply.github.com>
2020-06-10 00:25:16 -0400

    Update and rename styles.py to Qs and As.py

--- Qs and As.py
+++ Qs and As.py
@@ -1,4 +1,7 @@
+06/10/2020
 
+
+06/09/2020
 Question 1. Write a function for the sum of two numbers
 input : (1) non-empty array of distinct integers and (2) a target sum
 return : any couple of numbers that sum up to the target sum.
