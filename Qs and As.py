2c48824b17a76cc42d5bfeeafde716d11fbfb377
jinokwon <55212787+jinokwon@users.noreply.github.com>
2020-06-13 23:16:36 -0400

    Update Qs and As.py

--- Qs and As.py
+++ Qs and As.py
@@ -1,3 +1,23 @@
+
+06/13/2020
+[Question } Write a function that Finds a subsequence of an array
+(Ex) [1, 3, 5] is a subsequence of the array [-1, 1, 2, 3, 4, 5, 6]
+
+[ANS]
+from string import ascii_lowercase
+num_dict = {i:v for (i, v) in enumerate(ascii_lowercase)}
+dict_num = {v:i for (i, v) in enumerate(ascii_lowercase)}
+
+def caesar(string, key):
+	answer = []
+ 	for i in string:
+ 		withKey = dict_num[i] + key % 26
+ 		if withKey <=25:
+ 			answer.append(num_dict[withKey])
+ 		else:
+ 			answer.append(num_dict[withKey-26])
+ 	return " ".join(answer)
+
 06/11/2020
 [Question } Write a function that Finds a subsequence of an array
 (Ex) [1, 3, 5] is a subsequence of the array [-1, 1, 2, 3, 4, 5, 6]
@@ -12,10 +32,6 @@ def is_sub(array, sequence):
  		arr_idx += 1
  	# if the sequence index moved all the way to the last index ('len(sequence)'), it means 'sequence' is a subsequence.
  	return seq_idx == len(sequence)
-
-06/10/2020
-
-
 
 06/09/2020
 [Question 1] Write a function for the sum of two numbers
